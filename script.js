bfea5b601eb82e713fb1ce89016b7edc648463eb
adityahardaha07 <adityahardaha130@gmail.com>
2026-06-14 20:45:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ canvasCtx.fillText(
     35,
     80
 );
-if (fingers === 4 && 1 thumb) {
+if (fingers === 4 ) {
 
     canvasCtx.shadowColor = "#00ff00";
     canvasCtx.shadowBlur = 30;
@@ -90,7 +90,7 @@ if (fingers === 4 && 1 thumb) {
     canvasCtx.fillStyle = "#00ff00";
 
     canvasCtx.fillText(
-        "yayyyy a whole hand  ",
+        
         -200,
         0
     );
